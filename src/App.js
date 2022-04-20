b4169081d472ac874fce14fc670fba9dc703b2a1
Evan Fredericks <evan.fredericks@gmail.com>
2022-04-19 19:28:06 -0500

    solution part 8

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import { useSubscription, useApolloClient } from '@apollo/client'
+import Authors from './components/Authors'
+import Books from './components/Books'
+import NewBook from './components/NewBook'
+import Login from './components/Login'
+import Recommended from './components/Recommended'
+import { BOOK_ADDED, ALL_BOOKS } from './queries'
 
-function App() {
+const App = () => {
+  const [page, setPage] = useState('books')
+  const [token, setToken] = useState(null)
+  const client = useApolloClient()
+
+  useEffect(() => {
+    const token = localStorage.getItem('libraryToken')
+    if (token) {
+      setToken(token)
+    }
+  }, [])
+
+  const logout = () => {
+    setToken(null) 
+    localStorage.removeItem('libraryToken')
+  }
+
+  const updateCacheWith = (addedBook) => {
+    const includedIn = (set, object) => 
+      set.map(p => p.id).includes(object.id)  
+
+    const dataInStore = client.readQuery({ query: ALL_BOOKS })
+    if (!includedIn(dataInStore.allBooks, addedBook)) {
+      client.writeQuery({
+        query: ALL_BOOKS,
+        data: { allBooks : dataInStore.allBooks.concat(addedBook) }
+      })
+    }   
+  }
+
+  useSubscription(BOOK_ADDED, {
+    onSubscriptionData: ({ subscriptionData }) => {
+      const book = subscriptionData.data.bookAdded
+      alert('new book ' + book.title + ' by ' + book.author.name)
+      updateCacheWith(book)
+    }
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <button onClick={() => setPage('authors')}>authors</button>
+        <button onClick={() => setPage('books')}>books</button>
+        {token
+          ?<>
+            <button onClick={() => setPage('recommended')}>recommended</button>
+            <button onClick={() => setPage('add')}>add book</button>
+            <button onClick={logout}>logout</button>
+          </>
+          :<button onClick={() => setPage('login')}>login</button>
+        }
+        
+      </div>
+
+      <Authors show={page === 'authors'} />
+      <Books show={page === 'books'} />
+      <Recommended show={page === 'recommended'} />
+      <NewBook show={page === 'add'} />
+      <Login setToken={setToken} setPage={setPage} show={page === 'login'} />
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,17 +1,57 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import { 
+  ApolloClient, ApolloProvider, HttpLink, InMemoryCache, split
+} from '@apollo/client' 
+import { setContext } from 'apollo-link-context'
+
+import { getMainDefinition } from '@apollo/client/utilities'
+import { WebSocketLink } from '@apollo/link-ws'
+
+import App from './App'
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('libraryToken')
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `bearer ${token}` : null,
+    }
+  }
+})
+
+const httpLink = new HttpLink({
+  uri: 'http://localhost:4000',
+})
+
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000/graphql`,
+  options: {
+    reconnect: true
+  }
+})
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query)
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  authLink.concat(httpLink),
+)
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: splitLink
+})
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
